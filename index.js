76790db93d044efe81766f2347fb677e2534732e
Professor <92256188+StoreVia@users.noreply.github.com>
2023-07-05 16:50:41 +0530

    UPDATED

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
 const { ShardingManager } = require('discord.js');
-const manager = new ShardingManager('./bot.js', { token: `${process.env.token}` });
+const manager = new ShardingManager('./bot.js', { shardCount: auto, token: `${process.env.token}` });
 manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
 manager.spawn();
