36586133eb8a6f76ad887cba8033a7034bc3e77e
Professor <92256188+StoreVia@users.noreply.github.com>
2023-08-06 09:12:07 +0530

    UPDATED

--- commands/message/giveaway/gcreate.js
+++ commands/message/giveaway/gcreate.js
@@ -19,42 +19,20 @@ module.exports = class MessageGiveawayCreate extends Command {
         let duration = null;
         let winnercount = null;
         let hosted = null; */
-		let buttonRow = await client.functions.buttons(`Fill`, "gcfill", ButtonStyle.Secondary, "Cancel", "gccancel", ButtonStyle.Danger);
         let msgdefer = await client.functions.deferReply().message(message);
 
 		if(!await client.functions.permsCheck(`manageGuild`).message(message)){
             return msgdefer.edit({ content: `> You Need "Manage Guild" Permission To Use This Command.`});
         } else {
-			let msg = await msgdefer.edit({ content: `> Click Below Button To Fill Giveaway Details.`, components: [buttonRow] });
-			const filter = i => i.customId;
-			const collector = msg.createMessageComponentCollector({ filter, idle: 60000 })
-
-			collector.on('collect', async i => {
-				if(i.user.id != message.author.id){
-					return await i.reply({ content: "This Interaction Doesn't Belongs To You.", ephemeral: true });
-				} else if(i.customId === "gcfill"){
-					const description = new ModalBuilder()
-                		.setCustomId('myModalDescription')
-                		.setTitle('Ticket System Configuration.')
-                		.addComponents(
-							new ActionRowBuilder()
-								.addComponents(
-									new TextInputBuilder()
-                                		.setCustomId('text')
-                                		.setLabel("Set Ticket Panel Embed Description.")
-                                		.setStyle(TextInputStyle.Paragraph)))
-										
-				} else if(i.customId === "gccancel"){
-					buttonRow.components.map(component=> component.setDisabled(true));
-					return await i.update({ components: [buttonRow] })
-				}
-			})
-			collector.on('end', async (_, reason) => {
-				if(reason === 'idle' || reason === 'user'){
-					buttonRow.components.map(component=> component.setDisabled(true));
-					await msg.edit({ components: [buttonRow] });
-				}
-			});
+			let cancelled = false;
+    		let prize, channel, winnerCount, duration;
+			const filter = (m) => m.author.id === message.author.id
+			const collector = msgdefer.createMessageComponentCollector({ filter, time: 60000 });
+			let cancelEmbed = await client.functions.embedBuild().description(`Timeout! You Can't Respond To This Command. Please Use This Command Again.`).build();
+		}
+		async function waitingEmbed(msg, description){
+			let embed = await client.functions.embedBuild().description(description).build();
+			await msg.edit({ embeds: [embed] })
 		}
 	}
 };
