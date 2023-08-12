f9e0a1c5bfff74f65e6569ca50d51e9d897d3ee4
Professor <92256188+StoreVia@users.noreply.github.com>
2023-08-12 19:27:38 +0530

    UPDATED

--- C_Website/index.js
+++ C_Website/index.js
@@ -8,8 +8,19 @@ api.use("/", express.static(join(__dirname, "..", "html")));
 api.use(session({ secret: "GroBot", resave: true, saveUninitialized: false }));
 //apiInitializationEnd
 
+function redirectToHome(req, res, next) {
+  const allowedPaths = ["/", "/dashboard", "/mobile"];
+  const requestedPath = req.path;
+  if (!allowedPaths.includes(requestedPath)) {
+    res.sendFile(join(__dirname, "..", "html", "others", "error.html"));
+  } else {
+    next();
+  }
+}
+
 //RouterStart
 const router = express.Router();
+router.use(redirectToHome);
 router.get("/", (req, res) => {
   res.sendFile(join(__dirname, "..", "html", "index.html"));
 });
